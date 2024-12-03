f93826a657a004aa159769cfd52706181e60ffb0
unknown <kauetakada@gmail.com>
2024-12-03 14:41:09 -0300

    Finalização segundo curso AluraJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,10 +8,10 @@ const botoes = document.querySelectorAll('.app__card-button')
 const startPauseBt = document.querySelector('#start-pause')
 const tempoNaTela = document.querySelector('#timer')
 const musicaFocoInput = document.querySelector('#alternar-musica')
-const musica = new Audio('sons/luna-rise-part-one.mp3')
-const audioPlay = new Audio('./sons/play.wav');
-const audioPausa = new Audio('./sons/pause.mp3');
-const audioTempoFinalizado = new Audio('./sons/beep.mp3')
+const musica = new Audio('./assets/sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio('./assets/sons/play.wav');
+const audioPausa = new Audio('./assets/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./assets/sons/beep.mp3')
 const iniciarOuPausarBt = document.querySelector('#start-pause span')
 const iniciarOuPausarBtIcone = document.querySelector(".app__card-primary-butto-icon") 
 let intervaloId = null
@@ -50,7 +50,7 @@ function alterarContexto(contexto) {
         contexto.classList.remove('active')
 })
     html.setAttribute('data-contexto', contexto)
-    banner.setAttribute('src', `./imagens/${contexto}.png`)
+    banner.setAttribute('src', `./assets/imagens/${contexto}.png`)
     switch (contexto) {
         case "foco":
             titulo.innerHTML = `
@@ -73,15 +73,18 @@ function alterarContexto(contexto) {
 }
 
 const contagemRegressiva = () => {
-    if(tempoDecorridoEmSegundos <= 0) {
-        audioTempoFinalizado.play()  
-        alert('Tempo finalizado')
+    if(tempoDecorridoEmSegundos <= 0){
+        audioTempoFinalizado.play()
+        alert('Tempo finalizado!')
+        const focoAtivo = html.getAttribute('data-contexto') == 'foco'
+        if (focoAtivo) {
+            const evento = new CustomEvent('FocoFinalizado')
+            document.dispatchEvent(evento)
+        }
         zerar()
         return
     }
     tempoDecorridoEmSegundos -= 1
-    console.log('Tempo: ' + tempoDecorridoEmSegundos)
-    console.log('Id: ' + intervaloId)
     mostrarTempo()
 }
 
@@ -96,13 +99,13 @@ function iniciarOuPausar() {
     audioPlay.play();
     intervaloId = setInterval(contagemRegressiva, 1000)
      iniciarOuPausarBt.textContent = "Pausar"
-     iniciarOuPausarBtIcone.setAttribute('src', `./imagens/pause.png`)
+     iniciarOuPausarBtIcone.setAttribute('src', `./assets/imagens/pause.png`)
 }
 
 function zerar() {
     clearInterval(intervaloId) 
     iniciarOuPausarBt.textContent = "Começar"
-    iniciarOuPausarBtIcone.setAttribute('src', `./imagens/play_arrow.png`)
+    iniciarOuPausarBtIcone.setAttribute('src', `./assets/imagens/play_arrow.png`)
     intervaloId = null
 }
 
